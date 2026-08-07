150dbe9938d12c8abe1460dc2eacaf8b76cf80d6
Antoine <antoineblain43@gmail.com>
2026-08-07 14:52:16 +0200

    formatage black for pass CICD

--- ai/urls.py
+++ ai/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import llama_status, llama_chat
+from .views import llama_chat, llama_status
 
 urlpatterns = [
     path("status/", llama_status, name="llama_status"),
